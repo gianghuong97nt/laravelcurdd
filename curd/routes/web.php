d18431835963cb2e14ab241019d9d23ba1bb834e
giang <gianghuong97nt@gmail.com>
2018-12-25 18:17:09 +0700

    aa

--- curd/routes/web.php
+++ curd/routes/web.php
@@ -11,6 +11,53 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * Liệt kê danh sách
+ */
+Route::get('/','Category@index');
+Route::get('/product','Product@index');
+Route::get('/comment', 'Comment@index');
+
+
+/**
+ * Thêm danh sách
+ */
+Route::get('/category/create','Category@create');
+Route::get('/product/create','Product@create');
+Route::get('/comment/create', 'Comment@create');
+
+/**
+ * Sửa danh sách
+ */
+Route::get('/category/{id}/edit','Category@edit');
+Route::get('/product/{id}/edit','Product@edit');
+Route::get('/comment/{id}/edit','Comment@edit');
+
+/**
+ * Lưu trữ dữ liệu khi tạo mới
+ */
+Route::post('/category','Category@store');
+Route::post('/product','Product@store');
+Route::post('/comment','Comment@store');
+
+/**
+ * Lưu trữ dữ liệu khi sửa
+ */
+Route::post('/category/{id}','Category@update');
+Route::post('/product/{id}','Product@update');
+Route::post('/comment/{id}','Comment@update');
+
+/**
+ * Xóa danh sách
+ */
+Route::get('/category/{id}/delete','Category@delete');
+Route::get('/product/{id}/delete','Product@delete');
+Route::get('/comment/{id}/delete','Comment@delete');
+
+/**
+ * Lưu trũ dữ liệu khi xóa
+ */
+Route::post('/category/destroy/{id}','Category@destroy');
+Route::post('/product/destroy/{id}','Product@destroy');
+Route::post('/comment/destroy/{id}','Comment@destroy');
+
